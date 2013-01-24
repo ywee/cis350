0156e8ef447336ff7b970c5624d4295b61914c98
Yeong Wee <ywee@sas.upenn.edu>
2013-01-24 11:35:16 -0500

    1st commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Cis350::Application.config.secret_token = '826ba84631e19b23018b353f9aba2874789b70ede5da94988e2af34122c4c7c59bccff07f3e7383b95b4c4aa184d8a57cdc686fd66a294693124e80c235498db'
